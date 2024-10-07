0a5dc7552ee7cc9c61f5ae3a554b39b3f3c51e15
bohdan-vykhovanets <bohdan.vykhovanets@gmail.com>
2024-10-08 01:56:42 +0300

    second commit

--- Task5/MainPage.xaml.cs
+++ Task5/MainPage.xaml.cs
@@ -2,23 +2,49 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private List<Product> products;
 
         public MainPage()
         {
             InitializeComponent();
+            products = new List<Product>();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void AddProductClicked(object sender, EventArgs e)
         {
-            count++;
+            var newProduct = new Product { Id = products.Count + 1, Name = "New Product" };
+            products.Add(newProduct);
+            UpdateGrid();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void RemoveProductClicked(object sender, EventArgs e)
+        {
+            if (products.Any())
+            {
+                products.RemoveAt(products.Count - 1); // Remove the last product from the list
+                UpdateGrid();
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void UpdateGrid()
+        {
+            ProductsGrid.Children.Clear();
+            ProductsGrid.RowDefinitions.Clear();
+
+            ProductsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto});
+
+            ProductsGrid.Add(new Label { Text = "ID", FontAttributes = FontAttributes.Bold }, 0, 0);
+            ProductsGrid.Add(new Label { Text = "Name", FontAttributes = FontAttributes.Bold }, 1, 0);
+
+            int row = 1;
+            foreach (var product in products)
+            {
+                ProductsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+                ProductsGrid.Add(new Label { Text = product.Id.ToString() }, 0, row);
+                ProductsGrid.Add(new Label { Text = product.Name }, 1, row);
+                row++;
+            }
         }
     }
 }
--- Task5/MauiProgram.cs
+++ Task5/MauiProgram.cs
@@ -1,5 +1,29 @@
 ﻿namespace Task5
 {
+    class Product
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public DateOnly PackageDate { get; set; }
+        public string Description { get; set; }
+    }
+
+    class FoodProducts : Product
+    {
+        public DateOnly ExpirationDate { get; set; }
+        public int Quantity { get; set; }
+        public string Units { get; set; }
+    }
+
+    class Books : Product
+    {
+        public int Pages { get; set; }
+        public string Publisher { get; set; }
+        public string Author { get; set; }
+    }
+
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
