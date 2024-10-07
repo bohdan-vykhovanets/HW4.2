d2c7ddf9987be521aa2bf0983cb228bf1ccfb568
bohdan-vykhovanets <bohdan.vykhovanets@gmail.com>
2024-10-08 02:34:19 +0300

    Update Program.cs

--- Task4.1/Program.cs
+++ Task4.1/Program.cs
@@ -1,14 +1,23 @@
 ﻿interface IGeographicalObject
 {
+    private double _x;
+    private double _y;
+    private string _name;
+    private string _description;
 
+    public virtual void ShowInfo()
+    {
+
+    }
 }
 
 class River : IGeographicalObject
 {
-
+    private double _flowRate;
+    private double _length;
 }
 
 class Mountain : IGeographicalObject
 {
-
+    private string _highestPoint;
 }
